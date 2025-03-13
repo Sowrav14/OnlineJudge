3a52e14df0e99f78d48263b7ac453106f5ff36ad
Sowrav Nath <sowravnath05102002@gmail.com>
2025-03-13 20:12:53 +0600

    folder structure

---
+++ The_Judge/solutions/solution_1234/solution.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define Fast_IO() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+const int N = 1e5 + 10;
+const int M = 1e9 + 7;
+const int inf = 1e12;
+
+
+void solve(){
+
+    int n; cin >> n;
+    int sum = 0;
+    for(int i=0;i<n;i++){
+        int x;
+        sum += x;
+    }
+    cout << sum << endl;
+
+}
+
+
+signed main(){
+    Fast_IO()
+    int t = 1;
+    // cin >> t;
+    for(int i=1;i<=t;i++){
+        // cout << "Case #" << i << ": ";
+        solve();
+    }
+}
